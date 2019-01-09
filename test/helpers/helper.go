112c9b46e811851e6ef82db3ceda203f86e7ecfd
allen yuan <allen.yuan@oracle.com>
2019-01-09 21:50:00 +0800

    TM-314: add function to read vars from json config

--- test/helpers/helper.go
+++ test/helpers/helper.go
@@ -2,6 +2,7 @@ package helpers
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
 	"github.com/gruntwork-io/terratest/modules/ssh"
 	"github.com/gruntwork-io/terratest/modules/terraform"
@@ -65,12 +66,46 @@ func ReadTFVarsFile(terraformDirectory string) (TFVarsProperties, error) {
 	return config, nil
 }
 func GetKeyPairFromOptions(terraformOptions *terraform.Options, t *testing.T) (*ssh.KeyPair, error) {
+	_, hasSshAuthorizedKeys := terraformOptions.Vars["ssh_authorized_keys"]
+	_, hasSshPrivateKey := terraformOptions.Vars["ssh_private_key"]
+	if hasSshAuthorizedKeys && hasSshPrivateKey {
+		return GetKeyPairFromFiles(terraformOptions.Vars["ssh_authorized_keys"].(string), terraformOptions.Vars["ssh_private_key"].(string))
+	}
+
 	vars, err := ReadTFVarsFile(terraformOptions.TerraformDir)
 	if err != nil {
 		t.Error("Error while reading properties file")
 		return nil, err
 	}
-	//keyPair, err := test_helper.GetKeyPairFromFiles(vars["ssh_authorized_keys"], vars["ssh_private_key"])
 	keyPair, err := GetKeyPairFromFiles(vars["ssh_authorized_keys"], vars["ssh_private_key"])
 	return keyPair, err
+
+}
+func GetJsonConfig(configPath string, configuration interface{}) error {
+	raw, err := ioutil.ReadFile(configPath)
+	if err != nil {
+		return fmt.Errorf("Unable to read from configuration file: %s", err.Error())
+	}
+	err = json.Unmarshal(raw, &configuration)
+	if err != nil {
+		return fmt.Errorf("Failed to parse configurations: %s", err.Error())
+	}
+	return nil
+}
+func GetJsonVars(configuration interface{}) map[string]interface{} {
+	var result map[string]interface{}
+	m, _ := json.Marshal(configuration)
+	json.Unmarshal(m, &result)
+	return result
+}
+func MergeVars(maps ...map[string]interface{}) map[string]interface{} {
+	result := make(map[string]interface{})
+	for _, m := range maps {
+		for k, v := range m {
+			if v != "" && v != nil {
+				result[k] = v
+			}
+		}
+	}
+	return result
 }
